9ebf9b08db8c653add2aed2f5a7ce314794009ef
Anthony <Anthonarro@gmail.com>
2016-10-17 09:21:00 -0700

    Update Linux Compatibility
    Made compatible with school Linux server.

--- InventoryClass.cpp
+++ InventoryClass.cpp
@@ -4,10 +4,10 @@ using namespace std;
 
 class Inventory{
 private:
-	int m_itemNumber = 0;
-	int m_quantity = 0;
-	double m_cost = 0;
-	double m_totalCost = 0;
+	int m_itemNumber;
+	int m_quantity;
+	double m_cost;
+	double m_totalCost;
 
 public:
 	Inventory();
@@ -35,19 +35,19 @@ Inventory::Inventory(){
 
 Inventory::Inventory(int itemNum, int quantity, double cost){
 	if (itemNum < 0){
-		cout << "Value cannot be less than 0." << endl;
+		cout << "Input cannot be less than 0. Value unchanged." << endl;
 	}
 	else {
 		m_itemNumber = itemNum;
 	}
 	if (quantity < 0){
-		cout << "Value cannot be less than 0." << endl;
+		cout << "Input cannot be less than 0. Value unchanged." << endl;
 	}
 	else {
 		m_quantity = quantity;
 	}
 	if (cost < 0){
-		cout << "Value cannot be less than 0." << endl;
+		cout << "Input cannot be less than 0. Value unchanged." << endl;
 	}
 	else {
 		m_cost = cost;
@@ -57,7 +57,7 @@ Inventory::Inventory(int itemNum, int quantity, double cost){
 
 void Inventory::setItemNumber(int itemNum){
 	if (itemNum < 0){
-		cout << "Value cannot be less than 0." << endl;
+		cout << "Input cannot be less than 0. Value unchanged." << endl;
 	}
 	else {
 		m_itemNumber = itemNum;
@@ -66,7 +66,7 @@ void Inventory::setItemNumber(int itemNum){
 
 void Inventory::setQuantity(int quantity){
 	if (quantity < 0){
-		cout << "Value cannot be less than 0." << endl;
+		cout << "Input cannot be less than 0. Value unchanged." << endl;
 	}
 	else {
 		m_quantity = quantity;
@@ -75,7 +75,7 @@ void Inventory::setQuantity(int quantity){
 
 void Inventory::setCost(double cost){
 	if (cost < 0){
-		cout << "Value cannot be less than 0." << endl;
+		cout << "Input cannot be less than 0. Value unchanged." << endl;
 	}
 	else {
 		m_cost = cost;
@@ -110,16 +110,31 @@ int main(){
 
 	Inventory myItem;
 
+	cout << myItem.getItemNumber() << endl;
+	cout << myItem.getQuantity() << endl;
 	cout << myItem.getCost() << endl;
+	cout << myItem.getTotalCost() << endl;
+	myItem.setItemNumber(-5);
+	myItem.setQuantity(-32);
+	myItem.setCost(-4.99);
+	cout << myItem.getItemNumber() << endl;
+	cout << myItem.getQuantity() << endl;
+	cout << myItem.getCost() << endl;
+	cout << myItem.getTotalCost() << endl;
+	myItem.setItemNumber(5);
+	myItem.setQuantity(32);
 	myItem.setCost(4.99);
+	myItem.setTotalCost();
+	cout << myItem.getItemNumber() << endl;
+	cout << myItem.getQuantity() << endl;
 	cout << myItem.getCost() << endl;
+	cout << myItem.getTotalCost() << endl;
 
-	Inventory otherItem(-1, 12, 4.99);
+	Inventory otherItem(1, 12, 4.99);
 	cout << otherItem.getItemNumber() << endl;
 	cout << otherItem.getQuantity() << endl;
 	cout << otherItem.getCost() << endl;
 	cout << otherItem.getTotalCost() << endl;
 
-	system("pause");
 	return 0;
 }
