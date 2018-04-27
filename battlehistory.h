51cbd666dc78268ac0b154af6956ca2949dc9c09
SumWong1121 <sam961121@gmail.com>
2018-04-27 15:47:30 -0700

    Create battlehistory.h

---
+++ battlehistory.h
@@ -0,0 +1,8 @@
+#ifndef battlehistory_h
+#define battlehistory_h
+
+//create a list of the reward that the player get from the battles
+//make sure that player can know which battle got what
+
+//and design a battle history page to let player to check the records
+#endif
