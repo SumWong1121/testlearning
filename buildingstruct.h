4ca4dd2013e490872222db0f5c7acd32225800ef
SumWong1121 <sam961121@gmail.com>
2018-05-01 15:35:35 -0700

    Create buildingstruct.h

---
+++ buildingstruct.h
@@ -0,0 +1,7 @@
+//structure header file for all the thing that can be display in the map
+/*for now,we need:  forest
+                    river
+                    lake
+                    tree
+                    mountains
+                    hill*/
