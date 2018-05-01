2f54c17fb271e28b1e09934e3c7130f9fc2b3b07
Long Tong <32695047+jackhtong@users.noreply.github.com>
2018-05-01 16:21:12 -0700

    update structures

--- buildingstruct.h
+++ buildingstruct.h
@@ -9,3 +9,67 @@
 //things need to be included in the structure:1.total size, 2.designed char for the print out
 //declare a class to create a object for each of the structure(not print out)
 //use coordinate system to design the map by using switch case
+#ifndef BUILDINGSTRUCT_H
+#def BUILDINGSTRUCT_H
+
+#include <iostream>
+#include <cstdlib>
+
+struct symbol
+{
+	private:
+		char tree = '$';
+		char water = '~';
+		char hill = '^';
+		char bridge = '_';
+    public:
+		const char getchar(char input)
+		{
+			char b;
+			switch (input)
+			{
+				case "T": 
+					b = tree;
+					break;
+				case "W": 
+					b = water; 
+					break;
+				case "H":
+					b = water;
+					break;
+				case "B":
+					b = bridge;
+					break;
+			}
+			return b;
+		}
+};
+
+struct forest
+{	
+	
+	const std::size_t MAXIMUM = 100;
+	const std::size_t MINIMUM = 10;
+};
+
+struct mountains
+{
+	const std::size_t MAXIMUM = 10;
+	const std::size_t MINIMUM = 2;
+};
+
+struct river
+{
+	const std::size_t MAXIMUM = 150;
+	const std::size_t MINIMUM = 30
+};
+
+
+
+
+
+
+
+
+
+
