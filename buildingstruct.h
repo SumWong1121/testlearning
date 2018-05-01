d729c305d49a162b07102344dda3a00065653045
SumWong1121 <sam961121@gmail.com>
2018-05-01 15:53:29 -0700

    Update buildingstruct.h

--- buildingstruct.h
+++ buildingstruct.h
@@ -7,3 +7,5 @@
                     hill
                     doorway*/
 //things need to be included in the structure:1.total size, 2.designed char for the print out
+//declare a class to create a object for each of the structure(not print out)
+//use coordinate system to design the map by using switch case
